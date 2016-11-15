aaddfb51adb6540e700629d112e5a90f7d6d304d
darsh927 <darsh927@yahoo.com>
2016-11-14 20:43:11 -0500

    Commented HomeController / Mapped Region Sales / Merged each Region Sales into their respective values

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,6 +5,8 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import scala.io.Source
+import play.api.libs.functional.syntax._
+
 
 //import play.api.libs.json.JsValue // work on this, it might be a way to send json unhindered to views
 
@@ -24,30 +26,47 @@ import scala.io.Source
 
   //Can call this function to redirect to index page from wherever
   def redirect = Action { implicit request => Redirect(routes.HomeController.index()) }
- 
+  
+  //Importing North American Video Game Sales Data
   val source_na: String = Source.fromFile("app/assets/vgsales_na.json").getLines.mkString
   val json_na: JsValue = Json.parse(source_na)
 
+  //Importing European Video Game Sales Data
   val source_eu: String = Source.fromFile("app/assets/vgsales_eu.json").getLines.mkString
   val json_eu: JsValue = Json.parse(source_eu)
 
+  //Importing Japanese Video Game Sales Data
   val source_jp: String = Source.fromFile("app/assets/vgsales_jp.json").getLines.mkString
   val json_jp: JsValue = Json.parse(source_jp)
  
+  //Importing Other Regions Video Game Sales Data
   val source_other: String = Source.fromFile("app/assets/vgsales_other.json").getLines.mkString
   val json_other: JsValue = Json.parse(source_other)
  
+  //Importing Global Video Game Sales Data
   val source_global: String = Source.fromFile("app/assets/vgsales_global.json").getLines.mkString
   val json_global: JsValue = Json.parse(source_global)
  
+  //Importing All Video Game Sales Data
   val source: String = Source.fromFile("app/assets/videogames.json").getLines.mkString
   val json: JsValue = Json.parse(source)
 
-  val lat3 = json \\ "NA_Sales"
-  val test: JsValue = Json.toJson(lat3)
+  //Mapping Region Sales
+  val na_sales = (json \\ "NA_Sales").map(_.as[Double])
+  val eu_sales = (json \\ "EU_Sales").map(_.as[Double])
+  val jp_sales = (json \\ "JP_Sales").map(_.as[Double])
+  val other_sales = (json \\ "Other_Sales").map(_.as[Double])
+  val global_sales = (json \\ "Global_Sales").map(_.as[Double])
   
-  //Function to load JSON on page
-  def getNaSales = Action{ Ok(test) }
+  //Send these values to Index so that we can show them off in Graph format
+  val na_total = na_sales.sum
+  val eu_total = eu_sales.sum
+  val jp_total = jp_sales.sum
+  val other_total = other_sales.sum
+  val global_total = global_sales.sum
+  
+  //Functions to Send JSON to page
+  def getNaSales = Action{ Ok(Json.prettyPrint(json_eu)) }
   def getEuSales = Action{ Ok(Json.prettyPrint(json_eu)) }
   def getJpSales = Action{ Ok(Json.prettyPrint(json_jp)) }
   def getOtherSales = Action{ Ok(Json.prettyPrint(json_other)) }
